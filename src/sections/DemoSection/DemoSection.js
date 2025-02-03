0c4054731d752ad2675cd8e3ef88bdaeb4763e57
DavyHyun <dhyun1124@gmail.com>
2025-02-02 17:58:28 -0800

    template change

--- src/sections/DemoSection/DemoSection.js
+++ src/sections/DemoSection/DemoSection.js
@@ -1,9 +1,8 @@
 // Name it YOUR_NAMESection.js
 // Task:
-// 0. git checkout to your branch
-// 1. Add a picture of yourself, and a quick introduction paragraph
-// 2. Style it so it looks presentable somewhat
-// 3. Make something fun happen if you have time
+// 1. git checkout to your branch
+// 2. Add a picture of yourself, and a quick introduction paragraph
+// 3. Make something fun happen if you have time (button, text input)
 // 4. Commit to our git repository and make a PR
 
 import "./DemoSection.css"
