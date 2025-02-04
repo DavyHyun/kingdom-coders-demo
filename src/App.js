7f9a0d5175a271c50e020c3418f0cd0a0b947866
Christina <cpham5@uw.edu>
2025-02-03 18:37:44 -0800

    Christina's commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import DemoSection from './sections/DemoSection/DemoSection'
+import ChristinaSection from './sections/ChristinaPhamSection/ChristinaSection'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="content" style={{ marginTop: '56px' }}>
-          <DemoSection />
+          <ChristinaSection />
           <DemoSection />
           <DemoSection />
       </div>
