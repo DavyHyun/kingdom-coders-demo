b42d935f072d8219c7a607e6a8894bc7af38ac83
rhhhung <hungyi@uw.edu>
2025-02-03 18:36:09 -0800

    02/03/25 changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import DemoSection from './sections/DemoSection/DemoSection'
+import RachelSection from './sections/RachelSection/RachelSection'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="content" style={{ marginTop: '56px' }}>
-          <DemoSection />
+          <RachelSection />
           <DemoSection />
           <DemoSection />
       </div>
