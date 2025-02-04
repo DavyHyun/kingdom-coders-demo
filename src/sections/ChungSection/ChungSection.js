be13c5c142b557d26782bac6651ed7c0670906c4
Christian Chung <christianlchung@gmail.com>
2025-02-03 18:38:13 -0800

    added chung section lit

---
+++ src/sections/ChungSection/ChungSection.js
@@ -0,0 +1,20 @@
+// Name it YOUR_NAMESection.js
+// Task:
+// 1. git checkout to your branch
+// 2. Add a picture of yourself, and a quick introduction paragraph
+// 3. Make something fun happen if you have time (button, text input)
+// 4. Commit to our git repository and make a PR
+
+import "./ChungSection.css"
+import me from "./DSCF5272.jpg"
+import christmas from "./P1010064.jpg"
+
+export default function ChungSection() {
+  return (
+    <div className="section">
+      <h2>Chung</h2>
+      <img className="pfp" src= {me} alt="handsome man" />
+      <img className="christmas" src= {christmas} alt="me and my christmas gifts" />
+    </div>
+  );
+}
