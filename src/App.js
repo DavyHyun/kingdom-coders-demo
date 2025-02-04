4e7918d16633ab5b3d14e2d6356117527a9a5b50
chloe gao <chloe@chloes-MacBook-Air.local>
2025-02-03 18:36:09 -0800

    Chloe's changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import DemoSection from './sections/DemoSection/DemoSection'
+import ChloeSection from './sections/ChloeSection/ChloeSection';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="content" style={{ marginTop: '56px' }}>
-          <DemoSection />
+          <ChloeSection />
           <DemoSection />
           <DemoSection />
       </div>
