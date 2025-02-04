dbe94c9e2d5e0d9c590ea3ce9e7479095f04678e
derekdelacruz <derekdlc@uw.edu>
2025-02-03 18:37:11 -0800

    Derek's changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import DemoSection from './sections/DemoSection/DemoSection'
+import DerekSection from './sections/DerekSection/DerekSection'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="content" style={{ marginTop: '56px' }}>
-          <DemoSection />
+          <DerekSection />
           <DemoSection />
           <DemoSection />
       </div>
---
+++ src/sections/DerekSection/DerekSection.js
@@ -0,0 +1,18 @@
+// Name it YOUR_NAMESection.js
+// Task:
+// 1. git checkout to your branch
+// 2. Add a picture of yourself, and a quick introduction paragraph
+// 3. Make something fun happen if you have time (button, text input)
+// 4. Commit to our git repository and make a PR
+
+import "./DerekSection.css"
+import img from "./IMG_2228.JPG"
+
+export default function DerekSection() {
+  return (
+    <div className="section">
+      <h2>Derek's Section</h2>
+      <img className = "pfp" src={img} alt="Derek" />
+    </div>
+  );
+}
